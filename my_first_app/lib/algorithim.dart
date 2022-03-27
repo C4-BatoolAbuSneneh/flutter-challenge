1ad7a8f285b2eef4b94116404641430ea607fa77
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-03-27 11:52:12 +0300

    modify

--- my_first_app/lib/algorithim.dart
+++ my_first_app/lib/algorithim.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:avatar_glow/avatar_glow.dart';
+// import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:my_first_app/analyzer.dart';
